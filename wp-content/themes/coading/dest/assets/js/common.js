51c20762b8d83de8e435cce70d920adcbacc494d
wakai39@gmail.com <wakai39@gmail.com>
2024-04-10 21:59:27 +0900

    最近のマージをUP

--- wp-content/themes/coading/dest/assets/js/common.js
+++ wp-content/themes/coading/dest/assets/js/common.js
@@ -9,8 +9,10 @@
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
-//ハンバーガーメニュー
-$(function(){
+
+$(function () {
+  
+    //ハンバーガーメニュー
     const ham = $('#js-hamburger');
     const nav = $('#js-nav');
 
@@ -18,23 +20,30 @@ $(function(){
         ham.toggleClass('active'); // ハンバーガーメニューにactiveクラスを付け外し
         nav.toggleClass('active'); // ナビゲーションメニューにactiveクラスを付け外し
     });
+  
+      $(".linkon").on("click", function() {
+        ham.toggleClass('active'); // ハンバーガーメニューにactiveクラスを付け外し
+        nav.toggleClass('active'); // ナビゲーションメニューにactiveクラスを付け外し
+      })
+    // end ハンバーガーメニュー    
+  
+      //header アコーディオン(スマホのみ)
+      setAccordion();
+      if (window.matchMedia('(max-width:1080px)').matches) {
+        //SP初期は速報リンクのオープン
+        $('.def').next('nav.gnavi ul.small-list').slideToggle();
+        $('.def').toggleClass("open");
+        $('nav.gnavi ul.accordion>li.def').addClass("open");
+      }
 });
-// end ハンバーガーメニュー
 
-//header アコーディオン(スマホのみ)
-$(function() {
-  setAccordion();
-  if (window.matchMedia('(max-width:1080px)').matches) {
-    //SP初期は速報リンクのオープン
-    $('.def').next('nav.gnavi ul.small-list').slideToggle();
-    $('.def').toggleClass("open");
-    $('nav.gnavi ul.accordion>li.def').addClass("open");
-  }
-});
+
   //$(window).on("load orientationchange resize",function(){
-  $(window).on("orientationchange resize",function(){ //画面回転とリサイズで再ロード
+  
+  $(window).on("orientationchange",function(){ //画面回転とリサイズで再ロード
       location.reload(); 
   });
+
 
   function setAccordion(){
     //1080px以下の処理
@@ -54,20 +63,11 @@ $(function() {
 
 //ロード
 window.onload = function() {
-  //パラメタからtabのチェック
-  var obj2 = document.getElementsByName("c-tab")[0]; //nameの存在
-  if(obj2){
-  var n = parseInt(window.location.search.substr(1));
-  document.getElementsByName("c-tab")[n].checked = true;
-  }
-
-
 //読み込みが終わったらローディング非表示
   const spinner = document.getElementById('loading');
   // Add .loaded to .loading
   spinner.classList.add('loaded');
 }
-
 
 
 

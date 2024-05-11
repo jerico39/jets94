a0444dcea200bad0d9f1d328321f1d7d15d8d317
wakai39@gmail.com <wakai39@gmail.com>
2024-05-11 23:51:06 +0900

    ヘッダメニューの追加

---
+++ wp-content/themes/jets94-v5/inc/header_menu.php
@@ -0,0 +1,68 @@
+    <ul class="accordion empty"> <!--メニューがズレるので必要-->
+        <li>
+            <p>&nbsp;</p>
+        </li>
+    </ul>
+    <ul class="accordion">       
+        <li class="def head">
+        <a calss="head">メインメニュー</a>
+            <div class="i_box"><i class="one_i"></i></div>
+        </li>
+        <ul class="small-list">
+            <li><a href="/rss-all/" class="u-base-color u-font-bold ">NFLブログ更新情報</a></li>
+            <li><a href="/?s=">ニュース一覧</a></li>
+            <li><a href="/category/result-all/">試合結果</a></li>
+            <li><a href="/youtube-all/">Youtube更新情報</a></li>
+            <li><a href="/wwe/">あつまれWWEの森</a></li>
+            <li><a href="/about/">JETS狂の宴とは</a></li>
+        </ul>
+    </ul>
+    <ul class="accordion">       
+        <li class="head">
+            <a class="head">速報リンク</a>
+            <div class="i_box"><i class="one_i"></i></div>
+        </li>
+        <ul class="small-list">
+            <li><a href="https://nfljapan.com/broadcast" target="_blank">放送予定(NFL JAPAN)</a></li>
+            <li><a href="http://www.nfl.com/scores" target="_blank">試合速報/スコア(NFL.com)</a></li>
+            <li><a href="https://www.espn.com/nfl/scoreboard" target="_blank">スコア(ESPN)</a></li>
+            <li><a href="https://www.cbssports.com/nfl/scoreboard/" target="_blank">スコア(CBS)</a></li>
+            <li><a href="https://nfljapan.com/standing/division" target="_blank">順位表(NFL JAPAN) </a></li>
+            <li><a href="https://www.youtube.com/user/NFL" target="_blank">Youtube(NFL公式)</a></li>
+            <li><a href="http://www.tsp21.com/sports/nfl/" target="_blank">TSP SPORTS -NFL-</a></li>
+        </ul>
+    </ul>
+    <ul class="accordion">       
+        <li class="head">
+            <a class="head">オリジナルコンテンツ</a>
+            <div class="i_box"><i class="one_i"></i></div>
+        </li>
+        <ul class="small-list">
+            <li><a href="/roster/" class="u-base-color u-font-bold ">ジェッツ日本語ロースター</a></li>
+            <li><a href="/ontv/">NFLを見る方法</a></li>
+            <li><a href="/kasuga/">オードリー春日の呪い</a></li>
+            <li><a href="/eyeshield21/">特集アイシールド21</a></li>
+            <li><a href="/superbowl-what/">スーパーボウルとは？</a></li>
+            <li><a href="/nfl-word/">アメフト用語集</a></li>
+            <li><a href="/penalty/">NFL反則集</a></li>
+            <li ><a href="/#link-croud" >役に立つリンク集</a></li>
+            <li><a href="/ranking/">アクセスランキング</a></li>
+        </ul>
+    </ul>
+    <ul class="accordion">
+        <li class="head">
+            <a >人気のカテゴリ・タグ</a>
+            <div class="i_box"><i class="one_i"></i></div>
+        </li>
+        <ul class="small-list">
+            <li><a href="/tag/がっかり・オブ・ザ・イヤー/">がっかり・オブ・ザ・イヤー </a></li>
+            <li><a href="/tag/そうだったのか！/">そうだったのか！(NFLトリビア)</a></li>
+            <li><a href="/tag/スーパーボウル/">スーパーボウル</a></li>
+            <li><a href="/tag/ドラフト/">ドラフト</a></li>
+            <li><a href="/tag/nflスーパースター列伝/">NFLスーパースター列伝</a></li>
+            <li><a href="/tag/トム・ブレイディ/">トム・ブレイディ</a></li>
+            <li><a href="/tag/映画/">映画</a></li>
+            <li><a href="/tag/アイシールド21/">アイシールド21</a></li>
+            <li><a href="/tag/WWE/">WWE</a></li>
+        </ul>
+    </ul>

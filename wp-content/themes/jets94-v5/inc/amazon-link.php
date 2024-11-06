b733ad4ba7cff5f12642b4b9f7141d066479f1d5
wakai39@gmail.com <wakai39@gmail.com>
2024-11-06 23:01:50 +0900

    Amazonリンクの表示を6から８へ

--- wp-content/themes/jets94-v5/inc/amazon-link.php
+++ wp-content/themes/jets94-v5/inc/amazon-link.php
@@ -8,7 +8,7 @@ function amazon_box($place){
   $args = array(
     'post_type' => 'amazon',    // 投稿
     'post_status' => 'publish', // 公開された投稿、または固定ページを表示(デフォルト)
-    'posts_per_page' => 6,   // 表示する投稿数(-1を指定すると全投稿を表示)
+    'posts_per_page' => 8,   // 表示する投稿数(-1を指定すると全投稿を表示)
     'meta_query' => $metaq,	//検索条件
     'meta_key' => 'az_sort',// ソートキー
     'orderby' => 'meta_value_num', //ソートキーの扱い(数値型)

0dbcdb32ffa306a9f9bf56c641ed9301840faaa9
wakai39@gmail.com <wakai39@gmail.com>
2025-03-25 00:04:28 +0900

    タグクラウドを修正

--- wp-content/themes/jets94-v5/page-tag.php
+++ wp-content/themes/jets94-v5/page-tag.php
@@ -21,22 +21,33 @@ get_header();
 <div class="container single">
   <div class="l-page">
     <div class="l-main">
-    <section class="ad">
-          
-    </section>
-    <section class="ad">
-      <!-- google jets94-single-top -->
-      <ins class="adsbygoogle"
-        style="display:block"
-        data-ad-client="ca-pub-1827178535199750"
-        data-ad-slot="2853022806"
-        data-ad-format="auto"
-        data-full-width-responsive="true"></ins>
-      <script>
-      (adsbygoogle = window.adsbygoogle || []).push({});
-      </script>
-      <!-- //google -->
-    </section>
+    <!--↓Access ranking-->
+    <section class="row-slider ranking">
+    <div class="ttl">
+      <h3>今日のアクセスランキング</h3>
+      <a href="/ranking-all/">【ランキング一覧へ】</a>
+    </div>
+    <div class="inner">
+      <div class="lineup">
+        <?php
+                $wpp = array (
+                'range' => 'daily', /*集計期間の設定（daily,weekly,monthly）*/
+                'limit' => 5, /*表示数はmax5記事*/
+                'post_type' => 'post', /*投稿のみ指定） 'post,page' だと固定ページも含む*/
+                'title_length' => '20', /*タイトル文字数上限*/
+                'stats_comments' => '0', /*コメント数は非表示*/
+                'stats_views' => '0', /*閲覧数を表示させる=1*/
+                'thumbnail_width' => '150', /*画像のwidth（px）*/
+                'thumbnail_height' => '150', /*画像のheight（px）*/
+                'wpp_start' => '<ul class="lineup-carousel number">',
+	              'wpp_end' => '</ul>',
+                'post_html' => '<li>{thumb}<p>{title}</p></li>', /*表示されるhtmlの設定（サムネイル、記事タイトル、の順で表示）*/
+                ); ?>
+                <?php wpp_get_mostpopular($wpp); ?>
+      </div>
+    </div>
+  </section>
+  <!--↑Access ranking-->
     <h1>タグ一覧</h1>
     <section class="tag_cloud">
       <?php wp_tag_cloud('smallest=7 & largest=24 & number=80'); ?>

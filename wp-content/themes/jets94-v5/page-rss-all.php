a1f2b27e492378dd0f123a7ff4ed9659e28ca83e
wakai39@gmail.com <wakai39@gmail.com>
2025-03-25 14:00:44 +0900

    修正もれ

--- wp-content/themes/jets94-v5/page-rss-all.php
+++ wp-content/themes/jets94-v5/page-rss-all.php
@@ -8,19 +8,19 @@ Template Name:page-rss-all
 <div class="container">
   <div class="l-page">
     <div class="l-main">
-    <section class="ad">
-      <!-- google jets94-single-top -->
-      <ins class="adsbygoogle"
-        style="display:block"
-        data-ad-client="ca-pub-1827178535199750"
-        data-ad-slot="2853022806"
-        data-ad-format="auto"
-        data-full-width-responsive="true"></ins>
-      <script>
-      (adsbygoogle = window.adsbygoogle || []).push({});
-      </script>
-      <!-- //google -->
-      </section>
+        <section class="ad">
+          <!-- google jets94-single-top -->
+          <ins class="adsbygoogle"
+            style="display:block"
+            data-ad-client="ca-pub-1827178535199750"
+            data-ad-slot="2853022806"
+            data-ad-format="auto"
+            data-full-width-responsive="true"></ins>
+          <script>
+          (adsbygoogle = window.adsbygoogle || []).push({});
+          </script>
+          <!-- //google -->
+        </section>
 
         <section class="row-slider" id="amz">
           <div class="ttl"><h3>Amazonのオススメ[※ここから購入するとブログ継続・強化への寄付になります※]</h3></div>
@@ -31,68 +31,69 @@ Template Name:page-rss-all
               </ul>
             </div>
           </div>
-      </section>
+        </section>
 
-      <section class="rss-all">
-        <h1>NFL RSS</h1>
-        <p><a href="/youtube-all/">【NFL・アメフト系Youtube更新一覧】はこちら</a></p>
-        <div class="tabs">
-          <input type="radio" name="c-tab" checked id="all">
-          <label class="tab_item" for="all">NFL JAPAN</label>
-          <input id="programming" type="radio" name="c-tab">
-          <label class="tab_item" for="programming">ファンブログ</label>
-          <input id="design" type="radio" name="c-tab">
-          <label class="tab_item" for="design">海外NEWS</label>
-          <div class="tab_content" id="all_content">
-          <?php
-            $set_rss_code = "";
-            $set_rss_code = get_ini('rss.ini','NEWS_JP');
-            wprss_display_feed_items( $args = array(
-            'links_before' => '<ul class="list">',
-            'links_after' => '</ul>',
-            'link_before' => '<li>',
-            'link_after' => '</li>',
-            'limit' => '30',
-            'source' => $set_rss_code
-        ));
-        ?>
+        <section class="rss-all">
+          <h1>NFL RSS</h1>
+          <p><a href="/youtube-all/">【NFL・アメフト系Youtube更新一覧】はこちら</a></p>
+          <div class="tabs">
+            <input type="radio" name="c-tab" checked id="all">
+            <label class="tab_item" for="all">NFL JAPAN</label>
+            <input id="programming" type="radio" name="c-tab">
+            <label class="tab_item" for="programming">ファンブログ</label>
+            <input id="design" type="radio" name="c-tab">
+            <label class="tab_item" for="design">海外NEWS</label>
+            <div class="tab_content" id="all_content">
+            <?php
+              $set_rss_code = "";
+              $set_rss_code = get_ini('rss.ini','NEWS_JP');
+              wprss_display_feed_items( $args = array(
+              'links_before' => '<ul class="list">',
+              'links_after' => '</ul>',
+              'link_before' => '<li>',
+              'link_after' => '</li>',
+              'limit' => '30',
+              'source' => $set_rss_code
+              ));
+              ?>
+            </div>
+            <div class="tab_content" id="programming_content">
+              <?php
+              $set_rss_code = "";
+              $set_rss_code = get_ini('rss.ini','BLOG_JP');
+              wprss_display_feed_items( $args = array(
+              'links_before' => '<ul class="list">',
+              'links_after' => '</ul>',
+              'link_before' => '<li>',
+              'link_after' => '</li>',
+              'limit' => '35',
+              'source' => $set_rss_code
+              ));
+              ?>
+            </div>
+            <div class="tab_content" id="design_content">
+              <?php
+              $set_rss_code = "";
+              $set_rss_code = get_ini('rss.ini','NEWS_US');
+              wprss_display_feed_items( $args = array(
+              'links_before' => '<ul class="list">',
+              'links_after' => '</ul>',
+              'link_before' => '<li>',
+              'link_after' => '</li>',
+              'limit' => '35',
+              'source' => $set_rss_code
+              ));
+              ?>
+            </div>
           </div>
-          <div class="tab_content" id="programming_content">
-          <?php
-            $set_rss_code = "";
-            $set_rss_code = get_ini('rss.ini','BLOG_JP');
-            wprss_display_feed_items( $args = array(
-            'links_before' => '<ul class="list">',
-            'links_after' => '</ul>',
-            'link_before' => '<li>',
-            'link_after' => '</li>',
-            'limit' => '35',
-            'source' => $set_rss_code
-            ));
-            ?>
-          </div>
-          <div class="tab_content" id="design_content">
-          <?php
-        $set_rss_code = "";
-        $set_rss_code = get_ini('rss.ini','NEWS_US');
-        wprss_display_feed_items( $args = array(
-        'links_before' => '<ul class="list">',
-        'links_after' => '</ul>',
-        'link_before' => '<li>',
-        'link_after' => '</li>',
-        'limit' => '35',
-        'source' => $set_rss_code
-        ));
-        ?>
-          </div>
-        </div>
-    </section>
-    <section class="ad">
-          <?php get_template_part( 'inc/ad-under_refer'); ?>
+      </section>
+      <section class="ad">
+        <?php get_template_part( 'inc/ad-under_refer'); ?>
       </section>
     </div>
-      <div class="l-sidebar"> 
+    <div class="l-sidebar"> 
       <?php get_sidebar(); ?>
+    </div>
   </div>
 </div>
 <?php get_footer();?>

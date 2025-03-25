2943323df63ef152bb3fd62781cf1bbbcdf860bc
wakai39@gmail.com <wakai39@gmail.com>
2025-03-25 14:11:19 +0900

    tagcloudの設定を追加

--- wp-content/themes/jets94-v5/inc/head_setpage.php
+++ wp-content/themes/jets94-v5/inc/head_setpage.php
@@ -3,7 +3,7 @@
   $temp_name = basename($template); // パスの最後の名前（ファイル名）を取得
   //echo "tpname:".$temp_name;
   $search1 = '/front-page|wwe|single|index|rss-all|tagcloud/';
-  $search2 = '/front-page|wwe|rss-all/';
+  $search2 = '/front-page|wwe|rss-all|tagcloud/';
   $search3 = '/index/';
   //if(strpos($temp_name,'front-page') !== false){ //TOP
 if(preg_match($search1, $temp_name ) === 1){

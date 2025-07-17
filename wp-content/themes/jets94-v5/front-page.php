bc9122882d1c64aec650825eb61344cb8e28b7be
wakai39@gmail.com <wakai39@gmail.com>
2025-07-17 23:26:24 +0900

    修正

--- wp-content/themes/jets94-v5/front-page.php
+++ wp-content/themes/jets94-v5/front-page.php
@@ -1,8 +1,6 @@
 <?php get_header();
   ?>
-<style>
 
-</style>
 <script>
   var Obj
   jQuery(document).ready(function () {
@@ -51,6 +49,7 @@
             crossorigin="anonymous"></script>
         <!-- jets94-top-topix -->
         <ins class="adsbygoogle"
+           	style="display:block"
             data-ad-client="ca-pub-1827178535199750"
             data-ad-slot="6384907881"
             data-ad-format="auto"
@@ -149,7 +148,7 @@
             crossorigin="anonymous"></script>
         <!-- jets94-top-row02 -->
         <ins class="adsbygoogle"
-            style="display:inline-block;"
+            style="display:block"
             data-ad-client="ca-pub-1827178535199750"
             data-ad-slot="8909464840"
             data-ad-format="auto"

ec96fe32e4d39a2ce178fb263026b00c6bcb2fea
wakai39@gmail.com <wakai39@gmail.com>
2025-10-23 08:56:11 +0900

    記事のSNSリンクにコメント

--- wp-content/themes/jets94-v5/inc/page-sns.php
+++ wp-content/themes/jets94-v5/inc/page-sns.php
@@ -10,14 +10,14 @@ $article_url_encode = urlencode($article_url); // 記事URLエンコード
 $article_title_encode = urlencode($article_title." #nfljapan #dazn #nfl"); // 記事タイトルエンコード
 $url_encode=urlencode(get_permalink());
 $title_encode=urlencode(get_the_title());
-$tw_title_encode=urlencode(get_the_title()." #nfljapan #dazn #nfl");
+$tw_title_encode=urlencode(get_the_title()." #nfljapan #dazn #nfl\n※記事へは画像orURLをクリック\n");
 ?>
 <ul class="snsbtn">
   <li>
   <a href="http://www.facebook.com/sharer.php?src=bm&u=<?php echo $url_encode;?>&t=<?php echo $title_encode;?>" target="_blank"><i class="fab fa-facebook-square fa-2x"></i> </a>
   </li>
   <li>
-  <a href="http://twitter.com/intent/tweet?url=<?php echo $url_encode ?>&text=<?php echo $tw_title_encode ?>" target="_blank"><i class="fab fab fa-twitter-square fa-2x"></i> </a>
+  <a href="https://x.com/intent/post?url=<?php echo $url_encode ?>&text=<?php echo $tw_title_encode ?>" target="_blank"><i class="fab fab fa-twitter-square fa-2x"></i> </a>
   </li>
   <li>
   <a href="https://feedly.com/i/discover/sources/search/feed/https%3A%2F%2Fjets94.com%2F" target="_blank" class=""><i class="fas fa-rss-square fa-2x"></i></a>
